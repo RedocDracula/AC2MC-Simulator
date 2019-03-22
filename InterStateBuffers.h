83fe404af0d87a2b3f1120d5d35386a34935388f
Harshit Malik <harshit.malik999@gmail.com>
2019-03-23 00:53:08 +0530

    Add files via upload

---
+++ InterStateBuffers.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bitset>
+#define registerWidth 64
+
+class Register{
+	private:
+		int value;
+	public:
+		Register(){
+			value = 0;
+		}
+		
+		void reset(){
+			value = 0;
+		}
+		
+		void writeInt(int x){
+			value = x;
+		}
+
+		void writeBitset(bitset <registerWidth> x){
+			value = x.to_ulong();
+		}
+		
+		int readInt(){
+			return value;
+		}
+		
+		bitset<registerWidth> readBitset(){
+			bitset <registerWidth> x(value);	
+			return x;
+		}
+};
+
+class InterStateBuffers{
+	public:
+		Register RA, RB, RX, RY, RM;
+		
+		void resetAll(){
+			RA.reset();
+			RB.reset();
+			RX.reset();
+			RY.reset();
+			RM.reset();
+		}
+};
+	
+
